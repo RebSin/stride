850662c4bbc10a238364e807a55302897e81d5c3
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-11-05 20:09:12 -0800

    Created activities for diary and adding a new entry

--- app/src/main/java/com/example/stride/DiaryActivity.java
+++ app/src/main/java/com/example/stride/DiaryActivity.java
@@ -4,21 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DiaryActivity extends AppCompatActivity {
+public class DiaryActivity extends AppCompatActivity implements View.OnClickListener {
     //declare variables to use for the recycler view
     RecyclerView myRecycler;
     RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    Button addEntryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+        addEntryButton = (Button) findViewById(R.id.add_entry_button);
         myRecycler = (RecyclerView) findViewById(R.id.my_recycler_view);
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         myRecycler.setLayoutManager(mLayoutManager);
+        addEntryButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.add_entry_button){
+            Intent intent = new Intent(view.getContext(), AddDiaryEntryActivity.class);
+            startActivity(intent);
+        }
     }
 }
