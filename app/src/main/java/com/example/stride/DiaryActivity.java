cc36e1c64a3ec1e317332316d61e297a265518bd
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-11-05 19:41:11 -0800

    Basic Empty RecyclerView works

--- app/src/main/java/com/example/stride/DiaryActivity.java
+++ app/src/main/java/com/example/stride/DiaryActivity.java
@@ -1,14 +1,24 @@
 package com.example.stride;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class DiaryActivity extends AppCompatActivity {
+    //declare variables to use for the recycler view
+    RecyclerView myRecycler;
+    RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+        myRecycler = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        myRecycler.setLayoutManager(mLayoutManager);
     }
 }
