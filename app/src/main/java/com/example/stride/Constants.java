c601e3cb72d08ca2bd221738b3ba10bc40512e58
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-11-08 08:04:27 -0800

    Started making db tables, constants file and helper

---
+++ app/src/main/java/com/example/stride/Constants.java
@@ -0,0 +1,10 @@
+package com.example.stride;
+
+public class Constants {
+    public static final String DATABASE_NAME = "diarydatabase";
+    public static final String TABLE_NAME = "DIARYTABLE";
+    public static final String UID = "_id";
+    public static final String NAME = "Title";
+    public static final String TYPE = "Description";
+    public static final int DATABASE_VERSION = 1;
+}
