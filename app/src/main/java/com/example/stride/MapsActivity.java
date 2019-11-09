f41b23f400c2e75c60611bfb22cb5ab6495212b8
Rebecca <rebeccasingh98@gmail.com>
2019-11-08 21:21:27 -0800

    location permission asking, popup if user is still for one hour, button on map to display current user location, wrote function to be called when camera image saved to create markers on map

--- app/src/main/java/com/example/stride/MapsActivity.java
+++ app/src/main/java/com/example/stride/MapsActivity.java
@@ -1,19 +1,31 @@
 package com.example.stride;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,30 +33,37 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 //this displays our main homepage with the map
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener, View.OnClickListener,
-        OnMapReadyCallback {
+        OnMapReadyCallback, SensorEventListener {
     private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
     private Marker mBrisbane;
     private GoogleMap mMap;
     public Button searchButton;
     public Button diaryButton;
     public EditText searchMe;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        context = this;
         searchButton = (Button) findViewById(R.id.search_button);
         diaryButton = (Button) findViewById(R.id.diary_button);
         searchMe = (EditText) findViewById(R.id.searchEditText);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        searchButton.setOnClickListener(this);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        searchButton.setOnClickListener(this); //setting onclick listeners
         diaryButton.setOnClickListener(this);
         mapFragment.getMapAsync(this);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE); //accessing sensors for accelerometer
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     /**
@@ -56,30 +75,141 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
+
         // Add a marker in Sydney and move the camera
         mBrisbane = mMap.addMarker(new MarkerOptions()
                 .position(BRISBANE)
                 .title("Brisbane"));
         mBrisbane.setTag(0);
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
 
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation();
         // Set a listener for marker click.
         mMap.setOnMarkerClickListener(this);
     }
+
+///////////////////////////////////////////////////////
+
+    public double lastLatitude;
+    public double lastLongitude;
+    public LatLng thisLocHere;
+    public int tagNumber;
+
+    public void cameraActivatedSaveMarker(boolean photoTaken, String title) { //this is called
+        //if the camera has taken a photo and it takes the photo title
+        //it then reads the location of the device, which should be where the photo was taken
+        //then it sets a new marker to the map
+        if (photoTaken) {
+            thisLocHere = new LatLng(lastLatitude, lastLongitude);
+            Marker thisMarkerHere;
+
+            thisMarkerHere = mMap.addMarker(new MarkerOptions()
+                    .position(thisLocHere)
+                    .title(title));
+            thisMarkerHere.setTag(tagNumber);
+        }
+        tagNumber++;
+    }
+///////////////////////////////////////////////////////////////
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085); //default loc if permission not granted
+    private static final int DEFAULT_ZOOM = 15; //default zoom
+    private Location mLastKnownLocation;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private void getDeviceLocation() {
+        /*
+         * Get the location of the device if permission has been granted.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            lastLatitude = mLastKnownLocation.getLatitude();
+                            lastLongitude = mLastKnownLocation.getLongitude();
+
+                        } else {
+                            Log.d("MapsActivity", "Current location is null. Using defaults.");
+                            Log.e("MapsActivity", "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() { //this updates the location
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
     /** Called when the user clicks a marker. */
     @Override
     public boolean onMarkerClick(final Marker marker) {
@@ -121,5 +251,50 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
             startActivity(intent);
         }
     }
+    float motionValue; //holds current motion value
+    float prevMotion; //holds previous motion value
+    private float motionVal; //holds the motion value
+    boolean moving;
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            int xValue = (int) event.values[0]; //this retrieves the first value
+            int yValue = (int) event.values[1]; //this retrieves the second value
+            int zValue = (int) event.values[2]; //this retrieves the third value
+            prevMotion = motionValue; //previous motion is set to current motion
+            motionValue = (float) Math.sqrt(xValue * xValue + yValue * yValue + zValue * zValue);
+            //does math to get current motion value
+            float calc = motionValue - prevMotion; //gets the motion difference from original
+            motionVal = motionVal * 0.2f + calc; //multiplies prev motion and adds in the difference
+                if (motionVal > 0.1) { //if the device motion is higher
+                    //the user is moving.
+                    moving = true;
+                } else {
+                    moving = false;
+                    oneHourCountdown();
+                }
+        }
+    }
 
+    ////////////////////////////////////////////////////////
+    boolean oneHourHasPassed;
+    long originalTime = System.currentTimeMillis();
+    Context context;
+    public void oneHourCountdown() {
+        long nowTime = System.currentTimeMillis();
+        if(nowTime >= (originalTime + 3600*1000) && !moving) { //multiply by one hour in seconds by 1000 to get milliseconds
+            oneHourHasPassed = true;
+            originalTime = System.currentTimeMillis(); //update originalTime to new time
+            Toast toast = Toast.makeText(context, "You haven't moved in one hour!", Toast.LENGTH_SHORT);
+            toast.show(); //displays the toast
+        } else {
+            oneHourHasPassed = false;
+//            Log.d("MapsActivity", "Time has NOT passed");
+        }
+    }
+
+    ////////////////////////////////////////////////////
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
