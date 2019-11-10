8b24cf993a73b68aeccb8526150138dd53b32263
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-11-09 17:36:50 -0800

    Can now take a picture, however it is not stored in the db

--- app/src/main/java/com/example/stride/AddDiaryEntryActivity.java
+++ app/src/main/java/com/example/stride/AddDiaryEntryActivity.java
@@ -1,13 +1,28 @@
 package com.example.stride;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddDiaryEntryActivity extends AppCompatActivity implements View.OnClickListener{
     EditText title;
@@ -15,6 +30,10 @@ public class AddDiaryEntryActivity extends AppCompatActivity implements View.OnC
     RadioButton healthy;
     RadioButton unhealthy;
     RadioButton unsure;
+    Button take_photo_button;
+    ImageView image;
+    static final int Image_Capture_Code = 1;
+
     MyDatabase db;
     public String status;
 
@@ -28,12 +47,32 @@ public class AddDiaryEntryActivity extends AppCompatActivity implements View.OnC
         healthy = (RadioButton) findViewById(R.id.healthy_button);
         unhealthy = (RadioButton) findViewById(R.id.unhealthy_button);
         unsure = (RadioButton) findViewById(R.id.unsure_button);
+        take_photo_button = (Button) findViewById(R.id.picture_button);
+        image = (ImageView) findViewById(R.id.imgCapture);
+
+        take_photo_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cInt, Image_Capture_Code);
+            }
+        });
 
         healthy.setOnClickListener(this);
         unhealthy.setOnClickListener(this);
         unsure.setOnClickListener(this);
 
         db = new MyDatabase(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == Image_Capture_Code){
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(bp);
+        } else if(resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void addDiaryEntry(View view){
