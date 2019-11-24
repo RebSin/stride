7dafdc1f9700b975f966a51927be69ffda8599c9
Rebecca <rebeccasingh98@gmail.com>
2019-11-23 23:33:40 -0800

    Image now shows when marker clicked

--- app/src/main/java/com/example/stride/MarkerClickDetailResults.java
+++ app/src/main/java/com/example/stride/MarkerClickDetailResults.java
@@ -3,6 +3,7 @@ package com.example.stride;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 //this displays a page with all the details and saved image
 //after the user clicks on a marker in MapsActivity
@@ -35,9 +38,7 @@ public class MarkerClickDetailResults extends AppCompatActivity {
         String Status = db.getSelectedStatus(thisId); //get info from the database
         String Date = db.getSelectedDate(thisId);//get info from the database
         String Image = db.getSelectedImage(thisId);//get info from the database
-        Log.d("MarkerClick", "Image: " + Image);
 
-        //initalize and set values
         Bitmap temp = decodeBase64(Image);
         image = (ImageView) findViewById(R.id.detail_image);
         image.setImageBitmap(temp);
@@ -56,7 +57,8 @@ public class MarkerClickDetailResults extends AppCompatActivity {
     }
 
     public static Bitmap decodeBase64(String input) {
-        byte[] decodedByte = Base64.decode(input, 0);
+        byte[] decodedByte = Base64.decode(input, Base64.URL_SAFE); //use this flag
+        //because the string has '-' and without being flagged here, it doesn't work
         return BitmapFactory
                 .decodeByteArray(decodedByte, 0, decodedByte.length);
     }
